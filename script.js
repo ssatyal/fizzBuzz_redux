24ac7b93b7f461aab954e2f46060c058528f092c
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-16 16:32:01 -0500

    added solution

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var fizzBuzzRedux = function () {
-  // what arguments should add in above?
-  // How do you use the arguments to create fizzBuzz?;
+function fizzBuzz(wordOne, wordTwo, num)  {
+    for (var i = 1; i <= num; i++) {
+        if (i%3===0 && i%5===0) {
+            console.log(wordOne + wordTwo)}
+        else if (i%3===0) {
+            console.log(wordOne)}
+        else if (i%5===0) {
+            console.log(wordTwo)}
+        else {console.log(i)}
+    }
 }
+
+fizzBuzz("Fizz","Buzz",30)
